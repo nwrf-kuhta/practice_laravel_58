8f6156486f66200801d8e1b708ec14167774f8eb
nwrf-kuhta <nwrf.kuhta@gmail.com>
2020-03-22 04:12:55 +0900

    refs #2 User一覧への導線を作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <!-- MainMenu Links -->
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('users') }}">{{ __('Users') }}</a>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
