ba7eca5946b921f9194f5a024c6fefb85ee74975
nwrf-kuhta <nwrf.kuhta@gmail.com>
2020-03-22 23:48:29 +0900

    refs #9 疎通確認用APIを追加

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// 疎通確認API
+Route::middleware('auth:api')->group(function () {
+    Route::get('/hoge', 'Api\HogeController@hoge')->name('hoge');
 });
