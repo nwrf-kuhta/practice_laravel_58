a63d09be7916f344fb645b275eb028433e9c1edf
nwrf-kuhta <nwrf.kuhta@gmail.com>
2020-03-22 20:59:21 +0900

    refs #7 IDでユーザを取得するメソッド(find)を作成

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,11 +2,24 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
+
 /**
+ * usersに対してのデータ永続化に関するクラス
+ *
  * Class UserRepository
  * @package App\Repositories
  */
 class UserRepository
 {
-
+    /**
+     * 指定したIDのユーザを取得
+     *
+     * @param string $id
+     * @return User|null
+     */
+    public function find(string $id): ?User
+    {
+        return User::where('id', $id)->first();
+    }
 }
